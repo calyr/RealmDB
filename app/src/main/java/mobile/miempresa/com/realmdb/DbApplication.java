b8c47230be12ec194fae3d6efc5c8e9e207d1891
calyr <calyr.software@gmail.com>
2017-10-06 16:07:39 -0400

    Initial commit

---
+++ app/src/main/java/mobile/miempresa/com/realmdb/DbApplication.java
@@ -0,0 +1,20 @@
+package mobile.miempresa.com.realmdb;
+
+import android.app.Application;
+
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
+public class DbApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+
+        RealmConfiguration configuration = new RealmConfiguration.Builder()
+                .name("Mibasededatos.realm")
+                .build();
+
+        Realm.setDefaultConfiguration(configuration);
+    }
+}
